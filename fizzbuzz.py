fcb190393ab92759f34fc07c7f6203e8d16df91d
Alec Posney <alec@denialof.services>
2023-08-05 13:58:43 +1000

    Make more better

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,6 +5,21 @@ import argparse
 import openai
 
 
+def validate_fizz_buzz(counter: int, to_validate: str) -> None:
+    if counter % 15 == 0:
+        test_str = "FizzBuzz"
+    elif counter % 3 == 0:
+        test_str = "Fizz"
+    elif counter % 5 == 0:
+        test_str = "Buzz"
+    else:
+        test_str = str(counter)
+
+    assert (
+        to_validate == test_str
+    ), f"Invalid AI result, {to_validate} should have been {test_str}."
+
+
 def fizz_buzz(limit: int) -> Generator[str, None, None]:
     messages = [
         {
@@ -47,21 +62,33 @@ def fizz_buzz(limit: int) -> Generator[str, None, None]:
     )
 
     buffer = ""
+    ctr = 1
     for chunk in response:
         buffer += chunk.choices[0].delta.get("content", "")
         while "\n" in buffer:
             line, buffer = buffer.split("\n", 1)
-            yield line
+            yield line.strip()
     if buffer:
         yield buffer
 
 
 def main():
-    parser = argparse.ArgumentParser(description="A high performance FizzBuzz for the AI world")
-    parser.add_argument("limit", help="Limit for which to fizz buzz to.", type=int)
+    parser = argparse.ArgumentParser(
+        description="A high performance FizzBuzz for the AI world."
+    )
+    parser.add_argument(
+        "--validate", action="store_true", help="enable validation of FizzBuzz output"
+    )
+    parser.add_argument("limit", help="limit for which to fizz buzz to", type=int)
     args = parser.parse_args()
-    for value in fizz_buzz(args.limit):
-        print(value)
+    for index, value in enumerate(fizz_buzz(args.limit)):
+        print(value, end="")
+        if args.validate:
+            try:
+                validate_fizz_buzz(index + 1, value)
+            except AssertionError as e:
+                print(f", {e}")
+        print()
 
 
 if __name__ == "__main__":
